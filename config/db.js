59f23dbaa0374d414f44b2a3d2c4e4b9049bc996
cmoyano <cristianmoyano.mza@gmail.com>
2020-04-12 13:06:25 -0300

    Add db config

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017';
+
+const connectDB = async () => {
+	try {
+		await mongoose.connect(MONGO_URL, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true
+		});
+
+		console.log('MongoDB Connected...');
+	} catch (err) {
+		console.error(err.message);
+		// Exit process with failure
+		// process.exit(1);
+	}
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,34 +1,57 @@
 const {createServer} = require('http');
 const express = require('express');
+const connectDB = require('./config/db');
 const compression = require('compression');
 const morgan = require('morgan');
 const path = require('path');
+const helmet = require('helmet');
 
 const app = express();
+// NODE_ENV
 const dev = app.get('env') !== 'production';
 
 const normalizePort = port => parseInt(port, 10);
 const PORT = normalizePort(process.env.PORT || 5000);
 
+// Connect Database
+connectDB();
+
 if (!dev) {
+	// Middlewares
+	// Remove X-Powered-By header
 	app.disable('x-powered-by');
+	// Compression: Node.js compression middleware.
 	app.use(compression());
+	// Helmet: Express.js security with HTTP headers. 
+	app.use(helmet());
+	// Morgan: HTTP request logger middleware for node.js
 	app.use(morgan('common'));
+
+	// Statics
 	app.use(express.static(path.resolve(__dirname, 'build')));
 
+	// Routes
 	app.get('*', (req, res) =>{
 		res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
 	})
-
 }
-if (!dev) {
+if (dev) {
+	// Middlewares
 	app.use(morgan('dev'));
+	
+	// Statics
+	app.use(express.static(path.resolve(__dirname, 'build')));
+	
+	//Routes
+	app.get('*', (req, res) =>{
+		res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+	})
 }
 
 const server = createServer(app);
 
+// Run server with express: node server.js
 server.listen(PORT, err => {
 	if (err) throw err;
-
-	console.log('Server started!');
+	console.log(`Server started on port ${PORT}`);
 });
